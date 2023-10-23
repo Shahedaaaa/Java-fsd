f12c3382189a0576101c614ee5bc169c035ae065
shaheda <mahammadshaheda4@gmail.com>
2023-10-23 21:52:28 +0530

    First Commit

---
+++ src/pack1/Collections.java
@@ -0,0 +1,148 @@
+package pack1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.Vector;
+
+public class Collections 
+{
+	void ArrayList()
+	{
+		ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
+		list.add("Ravi");//Adding object in arraylist  
+		list.add("Vijay");  
+		list.add("Ravi");  
+		list.add("Ajay");  
+		//Traversing list through Iterator  
+		Iterator itr=list.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		} 
+		System.out.println("---------------------------");
+	}
+	void LinkedList()
+	{
+		LinkedList<String> al=new LinkedList<String>();  
+		al.add("Ravi");  
+		al.add("Vijay");  
+		al.add("Ravi");  
+		al.add("Ajay");  
+		Iterator<String> itr=al.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		} 
+		System.out.println("-------------------------------");
+	}
+	void Vector()
+	{
+		Vector<String> v=new Vector<String>();  
+		v.add("Ayush");  
+		v.add("Amit");  
+		v.add("Ashish");  
+		v.add("Garima");  
+		Iterator<String> itr=v.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		System.out.println("------------------------------");
+	}
+	void Stack()
+	{
+		Stack<String> stack = new Stack<String>();  
+		stack.push("Ayush");  
+		stack.push("Garvit");  
+		stack.push("Amit");  
+		stack.push("Ashish");  
+		stack.push("Garima");  
+		stack.pop();  
+		Iterator<String> itr=stack.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		System.out.println("----------------------------------");
+	}
+	void PriorityQueue()
+	{
+		PriorityQueue<String> queue=new PriorityQueue<String>();  
+		queue.add("Amit Sharma");  
+		queue.add("Vijay Raj");  
+		queue.add("JaiShankar");  
+		queue.add("Raj");  
+		System.out.println("head:"+queue.element());  
+		System.out.println("head:"+queue.peek());  
+		System.out.println("iterating the queue elements:");  
+		Iterator itr=queue.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		queue.remove();  
+		queue.poll();  
+		System.out.println("after removing two elements:");  
+		Iterator<String> itr2=queue.iterator();  
+		while(itr2.hasNext()){  
+		System.out.println(itr2.next());  
+		}  
+		System.out.println("--------------------------------");
+	}
+	void HashSet()
+	{
+		HashSet<String> set=new HashSet<String>();  
+		set.add("Ravi");  
+		set.add("Vijay");  
+		set.add("Ravi");  
+		set.add("Ajay");  
+		//Traversing elements  
+		Iterator<String> itr=set.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		System.out.println("------------------------");
+	}
+	void LinkedHashSet()
+	{
+		LinkedHashSet<String> set=new LinkedHashSet<String>();  
+		set.add("Ravi");  
+		set.add("Vijay");  
+		set.add("Ravi");  
+		set.add("Ajay");  
+		Iterator<String> itr=set.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		System.out.println("---------------------------");
+	}
+	void TreeSet()
+	{
+		TreeSet<String> set=new TreeSet<String>();  
+		set.add("Ravi");  
+		set.add("Vijay");  
+		set.add("Ravi");  
+		set.add("Ajay");  
+		//traversing elements  
+		Iterator<String> itr=set.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		System.out.println("------------------------------");
+	}
+	public static void main(String[] args) 
+	{
+		Collections c=new Collections();
+		c.ArrayList();
+		c.LinkedList();
+		c.Vector();
+		c.Stack();
+		c.HashSet();
+		c.LinkedHashSet();
+		c.PriorityQueue();
+		c.TreeSet();
+	}
+
+
+}
